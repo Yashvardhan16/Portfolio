de028c748dcf468f1c3b56bb73ae0e5fc70cbabe
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2023-08-05 21:33:55 +0530

    Add files via upload

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,14 +16,20 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-            <a href="https://www.linkedin.com/in/yashvardhan-singh-bhadauria-3461111a5/"><img src={navIcon1} alt="" /></a>
-                <a href="https://github.com/Yashvardhan16"><img src={navIcon2} alt="" /></a>
-                <a href="https://www.instagram.com/yashvardhxn/"><img src={navIcon3} alt="" /></a>
+              <a href="https://www.linkedin.com/in/yashvardhan-singh-bhadauria-3461111a5/">
+                <img src={navIcon1} alt="" />
+              </a>
+              <a href="https://github.com/Yashvardhan16">
+                <img src={navIcon2} alt="" />
+              </a>
+              <a href="https://www.instagram.com/yashvardhxn/">
+                <img src={navIcon3} alt="" />
+              </a>
             </div>
-            <p>Copyright 2022. All Rights Reserved</p>
+            <p>Copyright 2023. All Rights Reserved</p>
           </Col>
         </Row>
       </Container>
     </footer>
-  )
-}
+  );
+};
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -10,13 +10,11 @@ import projImg7 from "../assets/img/project-img7.png";
 import projImg8 from "../assets/img/project-img8.png";
 import projImg9 from "../assets/img/project-img9.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
-import 'animate.css';
-import TrackVisibility from 'react-on-screen';
+import "animate.css";
+import TrackVisibility from "react-on-screen";
 
 export const Projects = () => {
-
   const projects2 = [
-
     {
       title: "MultiThreaded Download Manager",
       description: "Multi Threading & OOPS",
@@ -32,7 +30,6 @@ export const Projects = () => {
       description: "Multi Threading & OOPS",
       imgUrl: projImg6,
     },
-   
   ];
   const projects1 = [
     {
@@ -69,90 +66,95 @@ export const Projects = () => {
     },
   ];
 
-  
   return (
     <section className="project" id="project">
       <Container>
         <Row>
           <Col size={12}>
             <TrackVisibility>
-              {({ isVisible }) =>
-              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
-                <h2>Projects</h2>
-                <p>Projects related to Web Development and Software Engineering</p>
-                <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
-                    <Nav.Item>
-                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
-                    </Nav.Item>
-                  </Nav>
-                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
-                    <Tab.Pane eventKey="first">
-                      <Row>
-                        {
-                          projects1.map((project1, index) => {
-                            return (
-                              <ProjectCard
-                              key={index}
-                              {...project1}
-                                />
-                            )
-                          })
-                        }
-                      </Row>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="second">  
-                      <Row>
-                        {
-                          projects2.map((project2, index) => {
-                            return (
-                              <ProjectCard
-                                key={index}
-                                {...project2}
-                                />
-                            )
-                          })
-                        }
-                      </Row>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="third">
-                      <Row>
-                        {
-                          projects3.map((project3, index) => {
-                            return (
-                              <ProjectCard
-                                key={index}
-                                {...project3}
-                                />
-                            )
-                          })
-                        }
-                      </Row>
-                    </Tab.Pane>
-                    
-                    <Tab.Pane eventKey="first">
-                      <p> Food Ordering Website Based on JavaScript,HTML5,CSS and PHP.</p>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="second">
-                      <p>MULTITHREADED DOWNLOAD MANAGER Based on Core Java Concepts Such as Multi-Theading and OOPS.</p>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="third">
-                      <p>Personal Health Monitoring Website Based On JavaScript,HTML5,CSS,SQL and Software Engineering concepts </p>
-                    </Tab.Pane>
-                  </Tab.Content>
-                </Tab.Container>
-              </div>}
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__fadeIn" : ""
+                  }
+                >
+                  <h2>College Projects</h2>
+                  <p>
+                    Projects related to Web Development and Software Engineering
+                  </p>
+                  <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                    <Nav
+                      variant="pills"
+                      className="nav-pills mb-5 justify-content-center align-items-center"
+                      id="pills-tab"
+                    >
+                      <Nav.Item>
+                        <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                      </Nav.Item>
+                    </Nav>
+                    <Tab.Content
+                      id="slideInUp"
+                      className={
+                        isVisible ? "animate__animated animate__slideInUp" : ""
+                      }
+                    >
+                      <Tab.Pane eventKey="first">
+                        <Row>
+                          {projects1.map((project1, index) => {
+                            return <ProjectCard key={index} {...project1} />;
+                          })}
+                        </Row>
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="second">
+                        <Row>
+                          {projects2.map((project2, index) => {
+                            return <ProjectCard key={index} {...project2} />;
+                          })}
+                        </Row>
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="third">
+                        <Row>
+                          {projects3.map((project3, index) => {
+                            return <ProjectCard key={index} {...project3} />;
+                          })}
+                        </Row>
+                      </Tab.Pane>
+
+                      <Tab.Pane eventKey="first">
+                        <p>
+                          {" "}
+                          Food Ordering Website Based on JavaScript,HTML5 and
+                          CSS.
+                        </p>
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="second">
+                        <p>
+                          MULTITHREADED DOWNLOAD MANAGER Based on Core Java
+                          Concepts Such as Multi-Theading and OOPS.
+                        </p>
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="third">
+                        <p>
+                          Personal Health Monitoring Website Based On
+                          JavaScript,HTML5,CSS,SQL and Software Engineering
+                          concepts{" "}
+                        </p>
+                      </Tab.Pane>
+                    </Tab.Content>
+                  </Tab.Container>
+                </div>
+              )}
             </TrackVisibility>
           </Col>
         </Row>
       </Container>
       <img className="background-image-right" src={colorSharp2}></img>
     </section>
-  )
-}
+  );
+};
